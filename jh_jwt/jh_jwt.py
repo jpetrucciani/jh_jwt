bf8aa0c559be214f35b49d38cbadf7f0c840d5ba
Jacobi Petrucciani <j@mimir.cloud>
2019-05-14 13:10:04 -0400

    removing hyphens from usernames as they cause issues

--- jh_jwt/jh_jwt.py
+++ jh_jwt/jh_jwt.py
@@ -1,39 +1,24 @@
 """
 jupyter jwt authentication class
 """
-from jupyterhub.handlers import (
-    BaseHandler
-)
-from jupyterhub.auth import (
-    Authenticator
-)
-from jupyterhub.auth import (
-    LocalAuthenticator
-)
-from jupyterhub.utils import (
-    url_path_join
-)
-from tornado import (
-    gen,
-    web
-)
-from traitlets import (
-    List,
-    Unicode
-)
-from jose import (
-    jwt
-)
+from jupyterhub.handlers import BaseHandler
+from jupyterhub.auth import Authenticator
+from jupyterhub.auth import LocalAuthenticator
+from jupyterhub.utils import url_path_join
+from tornado import gen, web
+from traitlets import List, Unicode
+from jose import jwt
 
 
 class JSONWebTokenLoginHandler(BaseHandler):
     """basic jwt login handler"""
+
     def get(self):
         header_name = self.authenticator.header_name
         param_name = self.authenticator.param_name
         post_login_url = self.authenticator.post_login_url
-        auth_header_content = self.request.headers.get(header_name, '')
-        auth_cookie_content = self.get_cookie('XSRF-TOKEN', '')
+        auth_header_content = self.request.headers.get(header_name, "")
+        auth_cookie_content = self.get_cookie("XSRF-TOKEN", "")
         signing_certificate = self.authenticator.signing_certificate
         secret = self.authenticator.secret
         username_claim_field = self.authenticator.username_claim_field
@@ -45,7 +30,7 @@ class JSONWebTokenLoginHandler(BaseHandler):
         elif auth_header_content:
             # we should not see 'token' as first word in the AUTHORIZATION header
             # if we do it could mean someone coming in with a stale API token
-            if auth_header_content.split()[0] != 'bearer':
+            if auth_header_content.split()[0] != "bearer":
                 raise web.HTTPError(403)
             token = auth_header_content.split()[1]
         elif auth_cookie_content:
@@ -82,25 +67,29 @@ class JSONWebTokenLoginHandler(BaseHandler):
     @staticmethod
     def verify_jwt_with_claims(token, signing_certificate, audience):
         # If no audience is supplied then assume we're not verifying the audience field.
-        if audience == '':
-            opts = {'verify_aud': False}
+        if audience == "":
+            opts = {"verify_aud": False}
         else:
             opts = {}
-        with open(signing_certificate, 'r') as rsa_public_key_file:
-            return jwt.decode(token, rsa_public_key_file.read(), audience=audience, options=opts)
+        with open(signing_certificate, "r") as rsa_public_key_file:
+            return jwt.decode(
+                token, rsa_public_key_file.read(), audience=audience, options=opts
+            )
 
     @staticmethod
     def verify_jwt_using_secret(json_web_token, secret):
         # If no audience is supplied then assume we're not verifying the audience field.
-        return jwt.decode(json_web_token, secret, algorithms=list(jwt.ALGORITHMS.SUPPORTED))
+        return jwt.decode(
+            json_web_token, secret, algorithms=list(jwt.ALGORITHMS.SUPPORTED)
+        )
 
     @staticmethod
     def retrieve_username(claims, username_claim_field):
         # retrieve the username from the claims
-        username = claims[username_claim_field]
-        if '@' in username:
+        username = claims[username_claim_field].replace("-", "")
+        if "@" in username:
             # process username as if email, pull out string before '@' symbol
-            return username.split('@')[0]
+            return username.split("@")[0]
 
         else:
             # assume not username and return the user
@@ -111,48 +100,49 @@ class JSONWebTokenAuthenticator(Authenticator):
     """
     Accept the authenticated JSON Web Token from header.
     """
+
     signing_certificate = Unicode(
         config=True,
         help="""
         The public certificate of the private key used to sign the incoming JSON Web Tokens.
 
         Should be a path to an X509 PEM format certificate filesystem.
-        """
+        """,
     )
 
     username_claim_field = Unicode(
-        default_value='upn',
+        default_value="upn",
         config=True,
         help="""
         The field in the claims that contains the user name. It can be either a straight username,
         of an email/userPrincipalName.
-        """
+        """,
     )
 
     post_login_url = Unicode(
-        default_value='home',
+        default_value="home",
         config=True,
         help="""
         Where to redirect the user after login
-        """
+        """,
     )
 
     expected_audience = Unicode(
-        default_value='',
+        default_value="",
         config=True,
-        help="""HTTP header to inspect for the authenticated JSON Web Token."""
+        help="""HTTP header to inspect for the authenticated JSON Web Token.""",
     )
 
     header_name = Unicode(
-        default_value='Authorization',
+        default_value="Authorization",
         config=True,
-        help="""HTTP header to inspect for the authenticated JSON Web Token."""
+        help="""HTTP header to inspect for the authenticated JSON Web Token.""",
     )
 
     param_name = Unicode(
         config=True,
-        default_value='access_token',
-        help="""The name of the query parameter used to specify the JWT token"""
+        default_value="access_token",
+        help="""The name of the query parameter used to specify the JWT token""",
     )
 
     secret = Unicode(
@@ -161,7 +151,7 @@ class JSONWebTokenAuthenticator(Authenticator):
         Shared secret key for siging JWT token.
 
         If defined, it overrides any setting for signing_certificate
-        """
+        """,
     )
 
     boolean_claims = List(
@@ -170,7 +160,7 @@ class JSONWebTokenAuthenticator(Authenticator):
         A list of JWT boolean claims that must be true to allow the authentication.
         Use it to validate that a claim has the True value.
         For example, 'jupyterhub_access' is set to true.
-        """
+        """,
     )
 
     boolean_negative_claims = List(
@@ -179,13 +169,11 @@ class JSONWebTokenAuthenticator(Authenticator):
         A list of JWT boolean claims that must be false to allow the authentication.
         Use it to validate that a claim as the False value.
         For example 'restricted' claim should be false.
-        """
+        """,
     )
 
     def get_handlers(self, app):
-        return [
-            (r'/login', JSONWebTokenLoginHandler),
-        ]
+        return [(r"/login", JSONWebTokenLoginHandler)]
 
     @gen.coroutine
     def authenticate(self, *args):
@@ -196,4 +184,5 @@ class JSONWebTokenLocalAuthenticator(JSONWebTokenAuthenticator, LocalAuthenticat
     """
     A version of JSONWebTokenAuthenticator that mixes in local system user creation
     """
+
     pass
